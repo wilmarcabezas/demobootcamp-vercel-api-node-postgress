354853737282d735bc70eff3438190187268b12b
wilmarcabezas <wilmar.cabezas.pisco@gmail.com>
2023-12-07 13:02:14 -0500

    Deploy 2

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const express = require('express');
 const { Pool } = require('pg');
 const app = express();
 app.use(express.json());
-app.use(apiKeyNice);
+//app.use(apiKeyNice);
 const port = 3000;
 
 const pool = new Pool({
